97e0c7f5742ffe550cd2bc5094e1adbbb9e5bd6c
Saurav Ganguly <58353139+SauravGanguly@users.noreply.github.com>
2020-02-02 12:16:41 +0530

    Update 01. print pixel using putpixel().cpp

--- 01. print pixel using putpixel().cpp
+++ 01. print pixel using putpixel().cpp
@@ -2,20 +2,20 @@
 #include <graphics.h>
 int main()
 {
+	int gd = DETECT, gm;
 	/* gm is Graphics mode which is a computer display mode that generates image using pixels. */
 	/* DETECT is a macro defined in "graphics.h" header file */
-	int gd = DETECT, gm;
-
-	/* initgraph initializes the graphics system by loading a graphics driver from disk */
 
 	initgraph(&gd, &gm, "");
+	/* initgraph initializes the graphics system by loading a graphics driver from disk */
 
-	putpixel(50,25,WHITE);
+    putpixel(50,25,WHITE);
+    /* The header file graphics.h contains putpixel() function which plots a pixel at location (x, y) of specified color. */
 
     getch();
 
-	/* closegraph function closes the graphics mode and deallocates all memory allocated by graphics system . */
 	closegraph();
+	/* closegraph function closes the graphics mode and deallocates all memory allocated by graphics system . */
 
 	return 0;
 
